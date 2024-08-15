fb4f5a0e3f03444bc360438af99f44c313f3107a
onboard repo action <onboard.repo@action.com>
2024-08-15 11:44:19 +0000

    Onboarded new repository 'gattma/argo-tests^'

--- renovate-config.js
+++ renovate-config.js
@@ -5,6 +5,7 @@ module.exports = {
         "Gepardec/renovate-playground",
         // "Gepardec/renovate-angular-playground"
          "gattma/argo-test",
+         "gattma/argo-tests^",
     ],
     prHourlyLimit: 0,
     recreateWhen: "always"
