1a13990c51753828d42c268fbb7beab7113d8103
Marko Gattringer <marko.gattringer@gepardec.com>
2024-08-15 10:39:58 +0200

    added workflow for repo onboarding

--- renovate-config.js
+++ renovate-config.js
@@ -6,15 +6,5 @@ module.exports = {
         // "Gepardec/renovate-angular-playground"
     ],
     prHourlyLimit: 0,
-    postUpgradeTasks: {
-        commands: ["/github-action/quarkus-update {{branchName}} {{currentVersion}} {{newVersion}} {{newMajor}}.{{newMinor}} {{platform}} {{repository}}"],
-        fileFilters: ["**/*", "**/.*"],
-        executionMode: "branch"
-    },
-    allowedPostUpgradeCommands: [
-        "^/github-action/quarkus-update {{branchName}} {{currentVersion}} {{newVersion}} {{newMajor}}.{{newMinor}} {{platform}} {{repository}}$",
-        "^npm ci --ignore-scripts$",
-        "^npx ng update {{{depName}}} --from={{{currentVersion}}} --to={{{newVersion}}} --migrate-only --allow-dirty --force$"
-    ],
     recreateWhen: "always"
 }
