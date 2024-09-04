4cb3c14d87ad50df5a254e3a8c0c4de31d416fdb
Marko <marko.gattringer@gepardec.com>
2024-09-04 10:45:36 +0200

    added demo repo

--- renovate-config.js
+++ renovate-config.js
@@ -2,7 +2,7 @@ module.exports = {
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     platform: 'github',
     repositories: [
-        // "Gepardec/renovate-playground",
+        "gattma/geparcedemy"
     ],
     prHourlyLimit: 0,
     recreateWhen: "always"
